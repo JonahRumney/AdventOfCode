f4ef99c89534b01eef523d9da3832e1f9e2ae864
Jonah Rumney <jonahrumney@gmail.com>
2025-05-08 21:53:42 -0400

    inital AoC code

---
+++ Day1/main.py
@@ -0,0 +1,18 @@
+import math
+
+with open("input.txt", "r") as file:
+    left = []
+    right = []
+    for line in file:
+        line = line.strip().split("   ")
+        print(line[0])
+        print(line[1])
+        left.append(int(line[0]))
+        right.append(int(line[1]))
+
+left.sort()
+right.sort()
+total = 0
+for i in range(len(left)):
+    total += abs(left[i] - right[i])
+print(total)
